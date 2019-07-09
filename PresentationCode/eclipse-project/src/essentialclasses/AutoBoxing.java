d27c32c46a0d99b82dba65c70d41c1e18497e231
cogi2ergosum@gmail.com <cogi2ergosum@gmail.com>
2019-07-09 15:36:33 +0800

    Add autoboxing example

--- PresentationCode/eclipse-project/src/essentialclasses/AutoBoxing.java
+++ PresentationCode/eclipse-project/src/essentialclasses/AutoBoxing.java
@@ -1,10 +1,14 @@
 package essentialclasses;
 
+import java.util.ArrayList;
+
 public class AutoBoxing {
 
 	public static void main(String[] args) {
 		Integer i = 5;
 		int j = new Integer(5);
+		ArrayList list = new ArrayList();
+		list.add(11111);
 	}
 
 }
